7bbe1885ca58fe7f532fa4d95eec22b164fd3953
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-03-27 00:04:53 -0400

    adds shaker component to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Shaker from "./components/shaker"
 
 function App() {
   return (
     <div className="App">
-      
+      <img src= "./images/magic8ball" alt="image of an magic 8gberg-ball"/>
+      <Shaker/>
     </div>
   );
 }
