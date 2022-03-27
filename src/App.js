52ab2167288d0813fc647e259f18593c432f2cb8
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-03-27 01:25:12 -0400

    adds white text centered on 8ball

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import './App.css';
 import questions from './components/Questions';
 import Shaker from "./components/shaker";
+import styledComponents from 'styled-components';
+import img from "./images/magic8ball.jpg"
 
 function App() {
   return (
     <div className="App">
       <select>{questions.map(question =><option value={question}>{question}</option>)}</select>
-      <img src={require("./images/magic8ball.jpg")} alt="8-ball"/>
+      <EightBall src={require("./images/magic8ball.jpg")} alt="8-ball">
+        <p>text inside</p>
+      </EightBall>
       <Shaker/>
     </div>
   );
 }
 
 export default App;
+
+const EightBall = styledComponents.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-position: center;
+  width: 50vw;
+  height: 33vh;
+  background-size: cover;
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  p{
+    -webkit-user-select: none; /* Safari */        
+    -moz-user-select: none; /* Firefox */
+    -ms-user-select: none; /* IE10+/Edge */
+    user-select: none; /* Standard */
+    color: white;
+  }
+`
