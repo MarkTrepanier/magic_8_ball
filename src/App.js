876ad1de6a4c50ef12c3a3040132512306ad1399
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-03-27 00:52:34 -0400

    adds questions selector

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Shaker from "./components/shaker"
+import questions from './components/Questions';
+import Shaker from "./components/shaker";
 
 function App() {
   return (
     <div className="App">
+      <select>{questions.map(question =><option value={question}>{question}</option>)}</select>
       <img src={require("./images/magic8ball.jpg")} alt="8-ball"/>
       <Shaker/>
     </div>
---
+++ src/components/Questions.js
@@ -0,0 +1,2 @@
+const questions = ['Will Eleven get her powers back?', 'Will Season 4 air this year?', 'Will Hopper be eaten by a demogorgon?', 'Will Dustin ever top Max in Dig Dug?'];
+export default questions;
