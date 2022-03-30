32e1c13d4dbb6d46447ba1d3403d4c2357009c24
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-03-30 13:28:40 -0400

    consolodates shaker, questions and 8-ball into one component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
-import questions from './components/Questions';
-import Shaker from "./components/shaker";
 import EightBall from './components/EightBall';
-import { useState } from 'react';
 
 function App() {
-  const [answer, setAnswer] = useState("What?")
-  const [shake, setShake] = useState(false);
 
   return (
     <div className="App">
-      <select>{questions.map(question => <option key={question} value={question}>{question}</option>)}</select>
-      <EightBall shake={shake} answer={answer} />
-      <Shaker setShake={setShake} setAnswer={setAnswer} />
+      <EightBall />
     </div>
   );
 }
--- src/components/EightBall.js
+++ src/components/EightBall.js
@@ -1,13 +1,37 @@
 import Styled from 'styled-components';
 import img from "../images/8-ball-back-small.png"
+import { useState } from 'react';
+
+let answers = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+    "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
+    "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
+    "Yes.", "Yes definitely.", "You may rely on it."]
+const questions = ['Will Eleven get her powers back?', 'Will Season 4 air this year?', 'Will Hopper be eaten by a demogorgon?', 'Will Dustin ever top Max in Dig Dug?'];
+
 
 function EightBall(props) {
+    const [answer, setAnswer] = useState("What?")
+    const [shake, setShake] = useState(false);
+
+    function shaker() {
+        setShake(true)
+        setAnswer(answers[Math.floor(Math.random() * answers.length)])
+        setTimeout(() => setShake(false), 1000)
+    }
 
     return (
-        <div className={props.shake ? "shake" : "8ball"}>
+        <div className={shake ? "shake" : "8ball"}>
+            <select>{questions.map(question => <option key={question.indexOf()} value={question}>{question}</option>)}</select>
             <EightBallStyle >
-                <p>{props.answer}</p>
+                <p>{answer}</p>
             </EightBallStyle>
+            <button onClick={() => {
+
+
+                shaker()
+            }}>
+                shake!
+            </button>
         </div>
     );
 }
